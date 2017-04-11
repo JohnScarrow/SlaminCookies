30bf16bd9801b9e198478d02b16189e85866a567
John Scarrow <johna@scarrows.com>
2017-04-11 12:38:13 -0700

    initial commit

--- sales.js
+++ sales.js
@@ -3,6 +3,8 @@
 var i = 0;
 var hours = ['6am','7am','8am','9am', '10am', '11am', '12pm','1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+
+
 //object example
 var store1stPike = {
   storeMin: 23,
@@ -16,6 +18,11 @@ var store1stPike = {
     }
   }
 };
+store1stPike.customerGen();
+for (i = 0; i < 15; i++) {
+  var predict1stPike = (hours[i] + ': ' + store1stPike.customerCapacity[i] + ' cookies');
+  console.log(predict1stPike);}
+console.log('Total:' + Math.sum([ store1stPike.customerCapacity]) + ' cookies');
 
 var storeSeatacAirport = {
   storeMin: 3,
@@ -77,8 +84,33 @@ var stores = [
   storeAlki
 ];
 
-var predict = [];
-i=0;
-for (i=0; i < 5; i++){
-  predict[i]=(hours[i]+' sold '+ stores[i].customerCapacity);
-}
+
+
+
+
+//Use as method on constructor
+
+
+// createElement
+//set Atrubutes
+//set content
+//
+stores.prototype.getTable = function() {
+  var table = document.createElement('table');
+  var titleRow = document.createElement('tr');
+  titleRow.textcontent = this.title;
+  table.aooendChild(tableRow);
+
+var itemRow, itemName, itemPrice
+  for (var i=0 i <this.items.length; i++){
+    itemRow = document.createElement('tr');
+    itemName = document.createElement('td');
+    itemName.textcontent = this.items[i].name;
+    itemPrice = document.createElement('td');
+    itemPrice.textcontent = this.items[i].price;
+    itemRow.appendChild(itemName);
+    itemRow.appendChild(itemPrice);
+    table.appendChild(itemRow);
+  }
+
+};
