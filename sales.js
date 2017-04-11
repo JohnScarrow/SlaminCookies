9cd56e3d314cc7c07693c98d3b5dc543c1db6679
John Scarrow <johna@scarrows.com>
2017-04-11 14:04:06 -0700

    finished for loop for html gen

--- sales.js
+++ sales.js
@@ -3,7 +3,7 @@
 var i = 0;
 var hours = ['6am','7am','8am','9am', '10am', '11am', '12pm','1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-
+var storeContent, pikeStoreTitle;
 
 //object example
 var store1stPike = {
@@ -12,17 +12,36 @@ var store1stPike = {
   storeAvg:6.3,
   customerCapacity:[],
   customerHours:hours,
+  storeContent:[],
   customerGen : function (){
     for (i = 0; i < 15; i++) {
-      this.customerCapacity.push(Math.floor(Math.random()*(this.storeMax-this.storeMin)+this.storeMin*this.storeAvg));
+      this.customerCapacity.push(Math.floor(Math.random()*(this.storeMax-this.storeMin)+this.storeMin*this.storeAvg))
+    }
+  },
+  total: store1stPike.customerCapacity.reduceRight(function(a,b){return a+b;})
+
+},
+
+  clientStoreInfo : function (){
+    for (i = 0; i < 15; i++){
+      storeContent[i] = document.createElement('div');
+      storeContent[i].textcontent = customerCapacity[i];
     }
   }
 };
 store1stPike.customerGen();
+
+pikeStoreTitle = document.createElement('h2');
+document.createElement('ul');
+pikeStoreTitle.textcontent = '1st and Pike';
+
+
 for (i = 0; i < 15; i++) {
+  pikeStoreTitle = document.createElement('li');
   var predict1stPike = (hours[i] + ': ' + store1stPike.customerCapacity[i] + ' cookies');
-  console.log(predict1stPike);}
-console.log('Total:' + Math.sum([ store1stPike.customerCapacity]) + ' cookies');
+  console.log(predict1stPike);
+  pikeStoreTitle.textcontent = store1stPike.customerCapacity[i];}
+console.log('Total:' + Math.(store1stPike.customerCapacity) + ' cookies');
 
 var storeSeatacAirport = {
   storeMin: 3,
@@ -83,34 +102,3 @@ var stores = [
   storeCapitalHill,
   storeAlki
 ];
-
-
-
-
-
-//Use as method on constructor
-
-
-// createElement
-//set Atrubutes
-//set content
-//
-stores.prototype.getTable = function() {
-  var table = document.createElement('table');
-  var titleRow = document.createElement('tr');
-  titleRow.textcontent = this.title;
-  table.aooendChild(tableRow);
-
-var itemRow, itemName, itemPrice
-  for (var i=0 i <this.items.length; i++){
-    itemRow = document.createElement('tr');
-    itemName = document.createElement('td');
-    itemName.textcontent = this.items[i].name;
-    itemPrice = document.createElement('td');
-    itemPrice.textcontent = this.items[i].price;
-    itemRow.appendChild(itemName);
-    itemRow.appendChild(itemPrice);
-    table.appendChild(itemRow);
-  }
-
-};
