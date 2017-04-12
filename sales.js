abb9c0abb27c18c723d5010960d4d688e134bfc4
John Scarrow <johna@scarrows.com>
2017-04-12 03:55:47 -0700

    Just starting constructors

--- sales.js
+++ sales.js
@@ -19,120 +19,20 @@ var store1stPike = {
     }
   }
 };
-store1stPike.customerGen();
-
-var pikeTotal = store1stPike.customerCapacity.reduce(function(a,b){return a+b;});
-
-for (i = 0; i < 15; i++) {
-  pikeStoreTitle = document.createElement('li');
-  var predict1stPike = (hours[i] + ': ' + store1stPike.customerCapacity[i] + ' cookies');
-  console.log(predict1stPike);
-  pikeStoreTitle.textcontent = store1stPike.customerCapacity[i];}
-console.log('Total:' + pikeTotal + ' cookies');
-
-
-var storeSeatacAirport = {
-  title:'seatac airport',
-  storeMin: 23,
-  storeMax: 65,
-  storeAvg:6.3,
-  customerHours:hours,
-  customerCapacity:[],
-  storeContent:[],
+function storeProp(title, min, max, avg){
+  title = '1st and pike',
+  this.storeMin = min,
+  storeMax = max,
+  storeAvg = avg,
+  customerHours = hours,
+  customerCapacity = [],
+  storeContent = [],
   customerGen : function (){
     for (i = 0; i < 15; i++) {
       this.customerCapacity.push(Math.floor(Math.random()*(this.storeMax-this.storeMin)+this.storeMin*this.storeAvg));
     }
   }
-};
-storeSeatacAirport.customerGen();
-
-var airportTotal = storeSeatacAirport.customerCapacity.reduce(function(a,b){return a+b;});
-
-for (i = 0; i < 15; i++) {
-  airportStoreTitle = document.createElement('li');
-  var predictAirport = (hours[i] + ': ' + storeSeatacAirport.customerCapacity[i] + ' cookies');
-  console.log(predictAirport);
-  airportStoreTitle.textcontent = storeSeatacAirport.customerCapacity[i];}
-console.log('Total:' + airportTotal + ' cookies');
-
-
-var storeSeattleCenter = {
-  title: 'seattle center',
-  storeMin: 23,
-  storeMax: 65,
-  storeAvg:6.3,
-  customerHours:hours,
-  customerCapacity:[],
-  storeContent:[],
-  customerGen : function (){
-    for (i = 0; i < 15; i++) {
-      this.customerCapacity.push(Math.floor(Math.random()*(this.storeMax-this.storeMin)+this.storeMin*this.storeAvg));
-    }
-  }
-};
-storeSeattleCenter.customerGen();
-
-var SeattleCenterTotal = storeSeattleCenter.customerCapacity.reduce(function(a,b){return a+b;});
-
-for (i = 0; i < 15; i++) {
-  seattleCenterStoreTitle = document.createElement('li');
-  var predictSeattleCenter = (hours[i] + ': ' + storeSeattleCenter.customerCapacity[i] + ' cookies');
-  console.log(predictSeattleCenter);
-  seattleCenterStoreTitle.textcontent = storeSeattleCenter.customerCapacity[i];}
-console.log('Total:' + SeattleCenterTotal + ' cookies');
-
-
-var storeCapitalHill = {
-  title: 'capital hill',
-  storeMin: 23,
-  storeMax: 65,
-  storeAvg:6.3,
-  customerHours:hours,
-  customerCapacity:[],
-  storeContent:[],
-  customerGen : function (){
-    for (i = 0; i < 15; i++) {
-      this.customerCapacity.push(Math.floor(Math.random()*(this.storeMax-this.storeMin)+this.storeMin*this.storeAvg));
-    }
-  }
-};
-storeCapitalHill.customerGen();
-
-var capitalHillTotal = storeCapitalHill.customerCapacity.reduce(function(a,b){return a+b;});
-
-for (i = 0; i < 15; i++) {
-  capitalHillStoreTitle = document.createElement('li');
-  var predictCapitalHill = (hours[i] + ': ' + storeCapitalHill.customerCapacity[i] + ' cookies');
-  console.log(predictCapitalHill);
-  capitalHillStoreTitle.textcontent = storeCapitalHill.customerCapacity[i];}
-console.log('Total:' + capitalHillTotal + ' cookies');
-
-
-var storeAlki = {
-  title: 'alki',
-  storeMin: 23,
-  storeMax: 65,
-  storeAvg:6.3,
-  customerHours:hours,
-  customerCapacity:[],
-  storeContent:[],
-  customerGen : function (){
-    for (i = 0; i < 15; i++) {
-      this.customerCapacity.push(Math.floor(Math.random()*(this.storeMax-this.storeMin)+this.storeMin*this.storeAvg));
-    }
-  }
-};
-storeAlki.customerGen();
-
-var AlkiTotal = storeAlki.customerCapacity.reduce(function(a,b){return a+b;});
-
-for (i = 0; i < 15; i++) {
-  alkiStoreTitle = document.createElement('li');
-  var predictAlki = (hours[i] + ': ' + storeAlki.customerCapacity[i] + ' cookies');
-  console.log(predictAlki);
-  alkiStoreTitle.textcontent = storeAlki.customerCapacity[i];}
-console.log('Total:' + AlkiTotal + ' cookies');
+}
 
 
 var stores = [store1stPike, storeSeatacAirport, storeSeattleCenter, storeCapitalHill, storeAlki];
