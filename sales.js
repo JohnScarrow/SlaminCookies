0a04b720376d3cc9f0327b4455155fcec8791fe5
John Scarrow <johna@scarrows.com>
2017-04-12 03:11:05 -0700

    Assignment-complete

--- sales.js
+++ sales.js
@@ -135,13 +135,7 @@ for (i = 0; i < 15; i++) {
 console.log('Total:' + AlkiTotal + ' cookies');
 
 
-var stores = [
-  store1stPike,
-  storeSeatacAirport,
-  storeSeattleCenter,
-  storeCapitalHill,
-  storeAlki
-];
+var stores = [store1stPike, storeSeatacAirport, storeSeattleCenter, storeCapitalHill, storeAlki];
 
 i = 0;
 var j = 0;
@@ -149,18 +143,53 @@ var storesUl, storesLi;
 
 var storesDiv = document.getElementById('stores');
 
-storesDiv.textContent = 'test';
+storesDiv.textContent = 'Slammin Cookies';
 
 
 for (i = 0; i < stores.length; i++){
   storesUl = document.createElement('ul');
-  storesUl.setAttribute('class', ('stores-Ul'+(i)));
+  storesUl.setAttribute('id', ('stores-Ul'+(i)));
   storesUl.textContent = stores[i].title;
   storesDiv.appendChild(storesUl);
-  for(j = 0; j < hours; j++){
-    storesLi = document.createElement('li');
-    storesLi.textContent = (stores[i].customerCapacity[j]);
-    storesLi.appendChild(storesUl+(i));
-
-  }
 }
+
+var storesUl0 = document.getElementById('stores-Ul0');
+for (j = 0; j < hours.length; j++){
+  storesLi = document.createElement('li');
+  storesLi.setAttribute('class', ('stores-Li'+(j)));
+  storesLi.textContent = hours[j] + ': ' + (stores[0].customerCapacity[j]) + 'cookies';
+  storesUl0.appendChild(storesLi);}
+
+var storesUl1 = document.getElementById('stores-Ul1');
+for (j = 0; j < hours.length; j++){
+  storesLi = document.createElement('li');
+  storesLi.setAttribute('class', ('stores-Li'+(j)));
+  storesLi.textContent = hours[j] + ': ' + (stores[1].customerCapacity[j]) + 'cookies';
+  storesUl1.appendChild(storesLi);}
+
+var storesUl2 = document.getElementById('stores-Ul2');
+for (j = 0; j < hours.length; j++){
+  storesLi = document.createElement('li');
+  storesLi.setAttribute('class', ('stores-Li'+(j)));
+  storesLi.textContent = hours[j] + ': ' + (stores[2].customerCapacity[j]) + 'cookies';
+  storesUl2.appendChild(storesLi);}
+
+var storesUl3 = document.getElementById('stores-Ul3');
+for (j = 0; j < hours.length; j++){
+  storesLi = document.createElement('li');
+  storesLi.setAttribute('class', ('stores-Li'+(j)));
+  storesLi.textContent = hours[j] + ': ' + (stores[3].customerCapacity[j]) + 'cookies';
+  storesUl3.appendChild(storesLi);}
+
+var storesUl4 = document.getElementById('stores-Ul4');
+for (j = 0; j < hours.length; j++){
+  storesLi = document.createElement('li');
+  storesLi.setAttribute('class', ('stores-Li'+(j)));
+  storesLi.textContent = hours[j] + ': ' + (stores[4].customerCapacity[j]) + 'cookies';
+  storesUl4.appendChild(storesLi);}
+
+//for(j = 0; j < hours; j++){
+  //storesLi = document.createElement('li');
+  //storesLi.textContent = (stores[i].customerCapacity[j]);
+  //storesUl0.appendChild(storesLi);
+//}
