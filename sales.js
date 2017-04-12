8dae8d3084e2e7b33c431ebfeb2e2033196cfa0f
John Scarrow <johna@scarrows.com>
2017-04-12 09:54:52 -0700

    seperating hours from data

--- sales.js
+++ sales.js
@@ -33,15 +33,33 @@ var stores = [store1stPike, storeSeatacAirport, storeSeattleCenter, storeCapital
 
 i = 0;
 var j = 0;
-var storesUl, storesLi;
+var storesUl, storesLi, storesTd;
 
 var storesDiv = document.getElementById('stores');
+var hoursDiv = document.getElementById('hours');
+var cookiesDiv = document.getElementById('cookies');
 
+storesDiv.textContent = 'Slammin Cookies';
+storesDiv.textContent = 'Slammin Cookies';
 storesDiv.textContent = 'Slammin Cookies';
 
 
 for (i = 0; i < stores.length; i++){
-  storesUl = document.createElement('ul');
+  storesUl = document.createElement('tr');
+  storesUl.setAttribute('id', ('stores-Ul'+(i)));
+  storesUl.textContent = stores[i].title;
+  storesDiv.appendChild(storesUl);
+}
+
+for (i = 0; i < stores.length; i++){
+  storesUl = document.createElement('tr');
+  storesUl.setAttribute('id', ('stores-Ul'+(i)));
+  storesUl.textContent = stores[i].title;
+  storesDiv.appendChild(storesUl);
+}
+
+for (i = 0; i < stores.length; i++){
+  storesUl = document.createElement('tr');
   storesUl.setAttribute('id', ('stores-Ul'+(i)));
   storesUl.textContent = stores[i].title;
   storesDiv.appendChild(storesUl);
@@ -49,41 +67,45 @@ for (i = 0; i < stores.length; i++){
 
 var storesUl0 = document.getElementById('stores-Ul0');
 for (j = 0; j < hours.length; j++){
-  storesLi = document.createElement('li');
+  storesLi = document.createElement('td');
   storesLi.setAttribute('class', ('stores-Li'+(j)));
-  storesLi.textContent = hours[j] + ': ' + (stores[0].customerCapacity[j]) + 'cookies';
-  storesUl0.appendChild(storesLi);}
+  storesLi.textContent = hours[j];
+  storesTd = document.createElement('td');
+  storesTd.setAttribute('class', ('stores-Td'+(j)));
+  storesTd.textContent = (stores[0].customerCapacity[j]) + 'cookies';
+  storesUl0.appendChild(storesLi);
+  storesUl0.appendChild(storesTd);}
 
 var storesUl1 = document.getElementById('stores-Ul1');
 for (j = 0; j < hours.length; j++){
-  storesLi = document.createElement('li');
+  storesLi = document.createElement('td');
   storesLi.setAttribute('class', ('stores-Li'+(j)));
   storesLi.textContent = hours[j] + ': ' + (stores[1].customerCapacity[j]) + 'cookies';
   storesUl1.appendChild(storesLi);}
 
 var storesUl2 = document.getElementById('stores-Ul2');
 for (j = 0; j < hours.length; j++){
-  storesLi = document.createElement('li');
+  storesLi = document.createElement('td');
   storesLi.setAttribute('class', ('stores-Li'+(j)));
   storesLi.textContent = hours[j] + ': ' + (stores[2].customerCapacity[j]) + 'cookies';
   storesUl2.appendChild(storesLi);}
 
 var storesUl3 = document.getElementById('stores-Ul3');
 for (j = 0; j < hours.length; j++){
-  storesLi = document.createElement('li');
+  storesLi = document.createElement('td');
   storesLi.setAttribute('class', ('stores-Li'+(j)));
   storesLi.textContent = hours[j] + ': ' + (stores[3].customerCapacity[j]) + 'cookies';
   storesUl3.appendChild(storesLi);}
 
 var storesUl4 = document.getElementById('stores-Ul4');
 for (j = 0; j < hours.length; j++){
-  storesLi = document.createElement('li');
+  storesLi = document.createElement('td');
   storesLi.setAttribute('class', ('stores-Li'+(j)));
   storesLi.textContent = hours[j] + ': ' + (stores[4].customerCapacity[j]) + 'cookies';
   storesUl4.appendChild(storesLi);}
 
 //for(j = 0; j < hours; j++){
-  //storesLi = document.createElement('li');
+  //storesLi = document.createElement('td');
   //storesLi.textContent = (stores[i].customerCapacity[j]);
   //storesUl0.appendChild(storesLi);
 //}
